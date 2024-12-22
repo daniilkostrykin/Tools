3837607ea32cec57b1a33323e5a8211b11a0962b
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2024-12-22 21:52:13 +0300

    split for download anf neuronets

---
+++ download/download.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".card-icon").forEach(function (icon) {
+    icon.addEventListener("click", function () {
+      const link = icon.closest(".card").querySelector("a");
+      if (link) {
+        window.open(link.href, "_blank");
+      }
+    });
+  });
+  
